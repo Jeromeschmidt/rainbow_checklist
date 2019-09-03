a741a92fa6dbf9d44fcf3e6391e6a3904fc86b0c
Jeromeschmidt <33012883+Jeromeschmidt@users.noreply.github.com>
2019-09-02 22:04:28 -0700

    Add files via upload
    final touches

--- checklist.py
+++ checklist.py
@@ -21,15 +21,18 @@ def destroy(index):
     # Destroy code here
     checklist.pop(index)
 
+#message to display error for improper index
 def error_message():
     print(bcolors.RED + "Uh-oh, looks like theres no item there!" + bcolors.END)
 
+#lists all items in checklist
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(bcolors.Yellow + "{} {}".format(index, list_item) + bcolors.END)
+        print(bcolors.YELLOW + "{} {}".format(index, list_item) + bcolors.END)
         index += 1
 
+#adds a checkmark to certain item
 def mark_completed(index):
     #Add code here that marks an item as completed
     i = 0
@@ -40,6 +43,7 @@ def mark_completed(index):
         i += 1
     list_all_items()
 
+#takes off checkmark for items
 def mark_uncompleted(index):
     #Add code here that marks an item as completed
     i = 0
@@ -50,6 +54,7 @@ def mark_uncompleted(index):
         i += 1
     list_all_items()
 
+#takes user input
 def user_input(prompt):
     # the input function will display a message in the terminal
     # and wait for user input.
@@ -88,11 +93,12 @@ def select(function_code):
         mark_completed(int(user_input("Mark which item is completed(by index):\n")))
         return True
 
+    #unchecks an item
     elif function_code == "X" or function_code == "x":
         list_all_items()
         mark_uncompleted(int(user_input("Mark which item do you want to uncheck?(by index):\n")))
         return True
-
+    #allows user to change certain item
     elif function_code == "U" or function_code == "u":
         list_all_items()
         to_change = int(user_input("Which item would you like to change?(by index):\n"))
@@ -101,9 +107,9 @@ def select(function_code):
         else:
             change_to = user_input("What would like to change it to?:\n")
             update(to_change, change_to);
-
         return True
 
+    #lets user delete item
     elif function_code == "D" or function_code == "d":
         if(0 >= len(checklist)):
             print(bcolors.RED + "Looks like the list is empty!" + bcolors.END)
@@ -117,48 +123,21 @@ def select(function_code):
                 destroy(deleteIndex)
             return True
 
-    elif function_code == "Q" or function_code == "q":
-        # This is where we want to stop our loop
-        return False
-
+    #lets user clear terminal
     elif function_code == "T" or function_code == "t":
         # This is where we want to stop our loop
         # found at https://stackoverflow.com/questions/2084508/clear-terminal-in-python
         print(chr(27) + "[2J")
         return True
+    #lets user exit program
+    elif function_code == "Q" or function_code == "q":
+        # This is where we want to stop our loop
+        return False
+
     # Catch all
     else:
         print("Unknown Option\n")
         return True
-
-def test():
-    # Add your testing code here
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-    destroy(1)
-
-    print(read(0))
-    #print(read(1))
-
-    list_all_items()
-
-    # Call your new function with the appropriate value
-    select("C")
-    # View the results
-    list_all_items()
-    # Call function with new value
-    select("R")
-    # View results
-    list_all_items()
-    # Continue until all code is run
-
-    user_value = user_input("Please Enter a value:")
-    print(user_value)
 
 #color help found at https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-terminal-in-python
 class bcolors:
@@ -166,8 +145,8 @@ class bcolors:
     YELLOW = '\033[93m'
     RED = '\033[91m'
     END = '\033[0m'
-#test()
-# QUESTION:
+
+#main running portion of program
 running = True
 while running:
     running = select(user_input(bcolors.GREEN + "Press: \n \
